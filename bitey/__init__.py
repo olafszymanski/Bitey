a55ce9ded49279e94f5fd0d8512aced0b19b4efc
Olaf Szymański <olaf.szym123@gmail.com>
2020-05-21 17:17:34 +0200

    Temporarily added SQLite database.

--- bitey/__init__.py
+++ bitey/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .config import DebugConfig
 
 
+db = SQLAlchemy()
+
+
 def create_app(config_class=DebugConfig):
-    app = Flask('bitey')
+    app = Flask('Bitey')
     app.config.from_object(config_class)
 
     from .main.views import main
 
     app.register_blueprint(main)
+
+    db.init_app(app)
 
     return app
--- bitey/config.py
+++ bitey/config.py
@@ -1,7 +1,8 @@
 class Config:
     DEBUG = False
     SECRET_KEY = '5f404452bae4eb7db44800a3d787cdb1'
-
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///bitey.sqlite3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DebugConfig(Config):
     DEBUG = True
