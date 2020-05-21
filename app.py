bba58bfcfce450b71e7d0d789db9bcf5bea242d9
Olaf Szymański <olaf.szym123@gmail.com>
2020-05-21 15:20:18 +0200

    Initial commit.

---
+++ app.py
@@ -0,0 +1,8 @@
+from bitey import create_app
+
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ bitey/config.py
@@ -0,0 +1,7 @@
+class Config:
+    DEBUG = False
+    SECRET_KEY = '5f404452bae4eb7db44800a3d787cdb1'
+
+
+class DebugConfig(Config):
+    DEBUG = True
